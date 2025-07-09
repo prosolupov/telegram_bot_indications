5c9d078025a1d287a5e0aeeed7f412582b118ae3
Prosolupov <snoob163@mail.ru>
2025-07-09 11:20:50 +0400

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,8 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import BotCommand, BotCommandScopeDefault, message
 
-#TOKEN = "5205949851:AAGZEvYAe-rdBIf90a7e-5qa633jMBCqXAE"
-TOKEN = "6649097753:AAHJepy_GHIvvlx5qCYyE_wwHQ7NF39IWCQ"
+
 
 storage = MemoryStorage()
 
